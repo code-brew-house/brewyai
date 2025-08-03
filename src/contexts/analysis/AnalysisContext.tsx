f6bda1ceaa5b9c7a35bb212be4e4ff438831ce15
Neha Rajesh <leehanseok3@gmail.com>
2025-08-03 14:49:00 +0530

    merge

---
+++ src/contexts/analysis/AnalysisContext.tsx
@@ -0,0 +1,7 @@
+import { AnalysisProviderType } from "./types";
+
+const AnalysisContext = createContext({
+  // state: Analysis,
+});
+
+export const AnalysisProvider = ({ children }: AnalysisProviderType) => {};
